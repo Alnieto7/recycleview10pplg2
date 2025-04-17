a1910a4efaff0a0583bf3f4809d3e50a8410f977
Alnieto7 <05532@smkrus.sch.id>
2025-04-17 09:35:58 +0700

    feet [SA] register

--- app/src/main/java/com/example/recycleview10pplg2/MainActivity.java
+++ app/src/main/java/com/example/recycleview10pplg2/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Menambahkan data menu makanan
+        // Menambahkan data menu makananstatus
         itemList = new ArrayList<>();
         itemList.add(new ItemModel("Nasi Goreng", "Rp 15.000", R.drawable.nasgor));
         itemList.add(new ItemModel("Nasi Goreng", "Rp 15.000", R.drawable.nasgor));
@@ -32,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         itemList.add(new ItemModel("Mie Goreng", "Rp 10.000", R.drawable.mi));
         itemList.add(new ItemModel("Seblak", "Rp 8.000", R.drawable.ceblak));
         itemList.add(new ItemModel("Seblak", "Rp 8.000", R.drawable.ceblak));
+        itemList.add(new ItemModel("Bakso", "Rp 12.000", R.drawable.ravatar));
+        itemList.add(new ItemModel("Bakso", "Rp 12.000", R.drawable.ravatar));
+        itemList.add(new ItemModel("Bakso", "Rp 12.000", R.drawable.ravatar));
+        itemList.add(new ItemModel("Bakso", "Rp 12.000", R.drawable.ravatar));
         itemList.add(new ItemModel("Bakso", "Rp 12.000", R.drawable.ravatar));
         itemList.add(new ItemModel("Bakso", "Rp 12.000", R.drawable.ravatar));
 
